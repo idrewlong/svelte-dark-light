ab3a2f81d21de0d2f041ef5c5c4fb37cbb0d7f56
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-03-06 19:57:50 -0600

    complete w/ toggle on nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+import "../app.css";
+import Nav from "$lib/nav.svelte";
+import "@friendofsvelte/toggle/styles/Toggle.css";
+</script>
+
+<Nav/>
+<slot />
+
+
+<style>
+        :global(.dark body) {
+                @apply bg-gray-900 text-white;
+        }
+        :global(body) {
+                @apply bg-gray-100 text-gray-900;
+        }
+</style>
